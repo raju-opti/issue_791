edbd30aa6cbda8b7391cf5a776746ba9db9c7bb1
Raju Ahmed <raju.ahmed@optimizely.com>
2023-06-16 18:48:51 +0600

    add optimizely instance check

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,48 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import optimizelySdk from '@optimizely/optimizely-sdk';
+import { v4 } from 'uuid';
+
+const loadOptimizely = async () => {
+  try {
+    console.log('uuid: ' + v4());
+
+    const optimizelyInstance = optimizelySdk.createInstance({
+      logLevel: 'error',
+      sdkKey: 'your-sdk-key',
+    });
+
+    if (!optimizelyInstance) {
+      throw new Error('Unable to create new Optimizely instance.');
+    }
+
+    const { success, reason } = await optimizelyInstance.onReady();
+
+    const userId = 'test-user-1';
+    const user = optimizelyInstance.createUserContext(userId);
+
+    if (!user) {
+      throw new Error(
+        `Error: Unable to create new Optimizely User Context for default user (${userId}).`,
+      );
+    }
+
+    const decision = user.decideAll();
+
+    console.log(decision);
+  } catch (e) {
+    console.error('Unable to load Optimizely.');
+    console.error(e);
+  }
+};
+
 function App() {
+  useEffect(() => {
+    loadOptimizely();
+  });
+
   return (
     <div className="App">
       <header className="App-header">
